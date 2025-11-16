1532144a4e8dcbb029e3f5c23bbfe131ed7e6dc3
Alex Schiftner <alex@shapediver.com>
2025-11-15 20:53:49 -0500

    first version of db query from selection

---
+++ Neo4jConnector.cs
@@ -0,0 +1,172 @@
+#r "nuget: Neo4j.Driver"
+// async: true
+
+using Neo4j.Driver;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper.Kernel;
+
+public class Neo4jConnector : IDisposable {
+
+    public Neo4jConnector(
+        string _uri = null, 
+        string _user = null, 
+        string _pass = null
+    )
+    {
+        var uri = _uri ?? Environment.GetEnvironmentVariable("NEO4J_URI");
+        var user = _user ?? Environment.GetEnvironmentVariable("NEO4J_USER");
+        var password = _pass ?? Environment.GetEnvironmentVariable("NEO4J_PASSWORD");
+
+        Driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        Session = Driver.AsyncSession();
+    }
+
+    public IAsyncSession Session {get;set;}
+
+    IDriver Driver;
+
+    // print debug info about a node
+    public void PrintDebugInfo(string key, INode node, bool indent = false)
+    {
+        var prefix = indent ? "\t" : ""; 
+        Console.WriteLine($"{prefix}Node {key}, Labels: {string.Join(",", node.Labels)}, Id {node.ElementId}");
+        foreach (var prop in node.Properties)
+        {
+            Console.WriteLine($"{prefix}\t{prop.Key} = {prop.Value}");
+        }
+    }
+
+    // print debug info about a relationship
+    public void PrintDebugInfo(string key, IRelationship rel, bool indent = false)
+    {
+        var prefix = indent ? "\t" : ""; 
+        Console.WriteLine($"{prefix}Relationship {key}, Type: {rel.Type}, Id {rel.ElementId}");
+        foreach (var prop in rel.Properties)
+        {
+            Console.WriteLine($"{prefix}\t{prop.Key} = {prop.Value}");
+        }
+    }
+
+    // print debug info about a path
+    public void PrintDebugInfo(string key, IPath path, bool indent = false)
+    {
+        var prefix = indent ? "\t" : ""; 
+        Console.WriteLine($"{prefix}Path {key}, Length: {path.Nodes.Count}");
+        for (int i=0; i<path.Nodes.Count-1; i++)
+        {
+            PrintDebugInfo("", path.Nodes[i], true);
+            PrintDebugInfo("", path.Relationships[i], true);
+        }
+        PrintDebugInfo("", path.End, true);
+    }
+
+    // run a query, optionally print debug info
+    public async Task<IReadOnlyList<IRecord>> RunQuery(string queryString, bool printDebugInfo = false)
+    {
+        var result = await Session.RunAsync(queryString);
+        var records = new List<IRecord>();
+
+        // Fetch records, optionally print debug info
+        await result.ForEachAsync(record =>
+        {
+            records.Add(record);
+            if (!printDebugInfo)
+                return;
+
+            // Print debug info about all fields in the record
+            for (int i = 0; i < record.Keys.Count; i++)
+            {
+                var key = record.Keys[i];
+                var value = record[i];
+                if (value is INode node) {
+                    PrintDebugInfo(key, node);
+                }
+                else if (value is IRelationship rel)
+                {
+                    PrintDebugInfo(key, rel);
+                }
+                else if (value is IPath path)
+                {
+                    PrintDebugInfo(key, path);
+                }
+                else {
+                    Console.Write($"{key} = {value}; ");
+                }
+            }
+            Console.WriteLine();
+        });
+
+        return records;
+    }
+
+    public void Dispose()
+    {
+        Driver?.Dispose();
+    }
+
+}
+
+
+public class QueryGenerator {
+
+    public QueryGenerator(Neo4jConnector connector)
+    {
+        Connector = connector;
+    }
+
+    Neo4jConnector Connector;
+
+    public async Task<IReadOnlyList<IRecord>> QueryFromTuples(IEnumerable<Tuple<IGH_DocumentObject, IGH_DocumentObject>> tuples)
+    {
+        // create a dictionary from componentinstance to variable name
+        var nodeVarDict = new Dictionary<IGH_DocumentObject, string>();
+        int i = 0;
+        foreach (var tuple in tuples)
+        {
+            var node = tuple.Item1;
+            if (!nodeVarDict.ContainsKey(node))
+                nodeVarDict.Add(node, $"n{i++}");
+            node = tuple.Item2;
+            if (!nodeVarDict.ContainsKey(node))
+                nodeVarDict.Add(node, $"n{i++}");
+        }
+
+        // create match clauses
+        i = 0;
+        var matches = tuples.Select(tuple => {
+            var v1 = nodeVarDict[tuple.Item1];
+            var v2 = nodeVarDict[tuple.Item1];
+            return $"p{i++}=({v1}:ComponentInstance)-[:Wire]->({v2}:ComponentInstance)";
+        });
+
+        // create where clauses
+        i = 0;
+        var wheres = nodeVarDict.Keys.Select(node => $"{nodeVarDict[node]} = '{node.ComponentGuid}'");
+
+        // create complete query
+        var queryString = $"MATCH {string.Join(", ", matches)} WHERE {string.Join(" AND ", wheres)} RETURN n1.VersionId, n1.PivotX, n2.PivotY";
+    
+        return await Connector.RunQuery(queryString, true);
+    }
+
+}
+
+
+// how to use the connector
+using (var connector = new Neo4jConnector())
+{
+    var query = @"MATCH p=(n:ComponentInstance)-[w:Wire]->(m:ComponentInstance) 
+    WHERE n.ComponentGuid = 'c9785b8e-2f30-4f90-8ee3-cca710f82402' 
+    AND m.ComponentGuid = '071c3940-a12d-4b77-bb23-42b5d3314a0d' 
+    RETURN p";
+
+    //var query = "MATCH (n:ComponentInstance) return n";
+    //var query = "show indexes";
+    await connector.RunQuery(query, true);
+ 
+}
+
+
---
+++ QuerySelectedGhComponentsFromNeo4j.cs
@@ -0,0 +1,232 @@
+#r "nuget: Neo4j.Driver"
+// async: true
+
+using Neo4j.Driver;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper.Kernel;
+using GraphHop2.Utilities;
+
+public class Neo4jConnector : IDisposable {
+
+    public Neo4jConnector(
+        string _uri = null, 
+        string _user = null, 
+        string _pass = null
+    )
+    {
+        var uri = _uri ?? Environment.GetEnvironmentVariable("NEO4J_URI");
+        var user = _user ?? Environment.GetEnvironmentVariable("NEO4J_USER");
+        var password = _pass ?? Environment.GetEnvironmentVariable("NEO4J_PASSWORD");
+
+        Driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        Session = Driver.AsyncSession();
+    }
+
+    public IAsyncSession Session {get;set;}
+
+    IDriver Driver;
+
+    // print debug info about a node
+    public void PrintDebugInfo(string key, INode node, bool indent = false)
+    {
+        var prefix = indent ? "\t" : ""; 
+        Console.WriteLine($"{prefix}Node {key}, Labels: {string.Join(",", node.Labels)}, Id {node.ElementId}");
+        foreach (var prop in node.Properties)
+        {
+            Console.WriteLine($"{prefix}\t{prop.Key} = {prop.Value}");
+        }
+    }
+
+    // print debug info about a relationship
+    public void PrintDebugInfo(string key, IRelationship rel, bool indent = false)
+    {
+        var prefix = indent ? "\t" : ""; 
+        Console.WriteLine($"{prefix}Relationship {key}, Type: {rel.Type}, Id {rel.ElementId}");
+        foreach (var prop in rel.Properties)
+        {
+            Console.WriteLine($"{prefix}\t{prop.Key} = {prop.Value}");
+        }
+    }
+
+    // print debug info about a path
+    public void PrintDebugInfo(string key, IPath path, bool indent = false)
+    {
+        var prefix = indent ? "\t" : ""; 
+        Console.WriteLine($"{prefix}Path {key}, Length: {path.Nodes.Count}");
+        for (int i=0; i<path.Nodes.Count-1; i++)
+        {
+            PrintDebugInfo("", path.Nodes[i], true);
+            PrintDebugInfo("", path.Relationships[i], true);
+        }
+        PrintDebugInfo("", path.End, true);
+    }
+
+    // run a query, optionally print debug info
+    public async Task<IReadOnlyList<IRecord>> RunQuery(string queryString, bool printDebugInfo = false)
+    {
+        var result = await Session.RunAsync(queryString);
+        var records = new List<IRecord>();
+
+        // Fetch records, optionally print debug info
+        await result.ForEachAsync(record =>
+        {
+            records.Add(record);
+            if (!printDebugInfo)
+                return;
+
+            // Print debug info about all fields in the record
+            for (int i = 0; i < record.Keys.Count; i++)
+            {
+                var key = record.Keys[i];
+                var value = record[i];
+                if (value is INode node) {
+                    PrintDebugInfo(key, node);
+                }
+                else if (value is IRelationship rel)
+                {
+                    PrintDebugInfo(key, rel);
+                }
+                else if (value is IPath path)
+                {
+                    PrintDebugInfo(key, path);
+                }
+                else {
+                    Console.Write($"{key} = {value}; ");
+                }
+            }
+            Console.WriteLine();
+        });
+
+        return records;
+    }
+
+    public void Dispose()
+    {
+        Driver?.Dispose();
+    }
+
+}
+
+
+public class QueryGenerator {
+
+    public QueryGenerator(Neo4jConnector connector)
+    {
+        Connector = connector;
+    }
+
+    Neo4jConnector Connector;
+
+    public async Task<IReadOnlyList<IRecord>> QueryFromTuples(List<(IGH_DocumentObject, IGH_DocumentObject)> tuples)
+    {
+        // create a dictionary from componentinstance to variable name
+        var nodeVarDict = new Dictionary<IGH_DocumentObject, string>();
+        int i = 0;
+        foreach (var tuple in tuples)
+        {
+            var node = tuple.Item1;
+            if (!nodeVarDict.ContainsKey(node))
+                nodeVarDict.Add(node, $"n{i++}");
+            node = tuple.Item2;
+            if (!nodeVarDict.ContainsKey(node))
+                nodeVarDict.Add(node, $"n{i++}");
+        }
+
+        // create match clauses
+        i = 0;
+        var matches = tuples.Select(tuple => {
+            var v1 = nodeVarDict[tuple.Item1];
+            var v2 = nodeVarDict[tuple.Item2];
+            return $"p{i++}=({v1}:ComponentInstance)-[:Wire]->({v2}:ComponentInstance)";
+        });
+
+        // create where clauses
+        i = 0;
+        var wheres = nodeVarDict.Keys.Select(node => $"{nodeVarDict[node]}.ComponentGuid = '{node.ComponentGuid}'");
+
+        // create complete query
+        var queryString = $"MATCH {string.Join(", ", matches)} WHERE {string.Join(" AND ", wheres)} RETURN n1.VersionId, n1.PivotX, n1.PivotY";
+        Console.WriteLine(queryString);
+        
+        return await Connector.RunQuery(queryString, true);
+    }
+
+}
+
+List<(IGH_DocumentObject, IGH_DocumentObject)> GetConnections(IEnumerable<IGH_DocumentObject> selectedObjects)
+{
+    var connections = new List<(IGH_DocumentObject, IGH_DocumentObject)>();
+
+    foreach (IGH_DocumentObject obj in selectedObjects)
+    {
+        if (obj is IGH_Component component)
+        {
+            // Output connections (this component to recipients)
+            foreach (var outputParam in component.Params.Output)
+            {
+                foreach (IGH_Param recipient in outputParam.Recipients)
+                {
+                    IGH_DocumentObject recipientComponent = recipient.Attributes.GetTopLevel.DocObject;
+                    if (selectedObjects.Contains(recipientComponent))
+                        connections.Add((obj, recipientComponent));
+                }
+            }
+        }
+    }
+
+    return connections;
+}
+
+List<(IGH_DocumentObject, IGH_DocumentObject)> GetSelectedTuples()
+{
+    var empty = new List<(IGH_DocumentObject, IGH_DocumentObject)>();
+
+    if (Grasshopper.Instances.ActiveCanvas == null)
+    {
+        Console.WriteLine("Grasshopper is not loaded or no canvas is active!");
+        return empty;
+    }
+
+    var ghDoc = Grasshopper.Instances.ActiveCanvas.Document;
+    if (ghDoc == null)
+    {
+        Console.WriteLine("No active Grasshopper document found!");
+        return empty;
+    }
+
+    var selectedObjects = ghDoc.SelectedObjects();
+    if (selectedObjects.Count == 0)
+    {
+        Console.WriteLine("No Grasshopper components selected!");
+        return empty;
+    }
+
+    var connections = GetConnections(selectedObjects);
+
+    if (connections.Count == 0)
+    {
+        Console.WriteLine("No connections found for selected components.");
+        return empty;
+    }
+
+    Console.WriteLine($"Number of nodes/connections found: {selectedObjects.Count}/{connections.Count}");
+
+    return connections;
+}
+
+
+using (var connector = new Neo4jConnector())
+{
+    var tuples = GetSelectedTuples();
+    if (tuples.Count == 0)
+        return;
+
+    var generator = new QueryGenerator(connector);
+    await generator.QueryFromTuples(tuples);
+
+}
+
+    
